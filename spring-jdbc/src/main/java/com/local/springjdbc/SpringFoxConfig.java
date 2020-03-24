7c423faa46aa0a30d87260b13ea3a19a2728e67c
root <root@localhost.localdomain>
2020-03-24 08:26:42 +0800

    Added custom ApiInfo in SpringFoxConfig

--- spring-jdbc/src/main/java/com/local/springjdbc/SpringFoxConfig.java
+++ spring-jdbc/src/main/java/com/local/springjdbc/SpringFoxConfig.java
@@ -1,5 +1,8 @@
 package com.local.springjdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -7,6 +10,9 @@ import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -22,7 +28,17 @@ class SpringFoxConfig {
 					.select()
 					.apis(RequestHandlerSelectors.basePackage("com.local.springjdbc.controller"))	// .apis(RequestHandlerSelectors.any())
 					.paths(PathSelectors.any())	//.paths(PathSelectors.ant("/school/***"))
-					.build();
+					.build()
+				.apiInfo(apiInfo());
+	}
+	
+	private ApiInfo apiInfo() {
+	       ApiInfo apiInfo = new ApiInfo(
+	      "School RESTful WebService", 
+	      "This pages documents School RESTful Web Service endpoints", "1.0",
+	      "http://www.houwaey.com/service.html", new Contact("Houwaey Ford", "http://www.houwaey.com", "houwaey@gmail.com"), 
+	      "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<VendorExtension>());
+		return apiInfo;
 	}
 	
 }
